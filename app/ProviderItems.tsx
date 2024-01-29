1784c410e3e234965ce06a35de39db1b42b112d6
Fabian Ajokubi <favourajokubi@gmail.com>
2024-01-29 15:37:48 +0100

    remove commented out lines

--- app/ProviderItems.tsx
+++ app/ProviderItems.tsx
@@ -3,12 +3,10 @@
 import ProviderComponent from "@/redux-store/provider";
 import ResultsSection from "./components/ResultsSection";
 import SearchSection from "./components/SearchSection";
-// import Test from "./components/Test"
 
 export default function ProviderItems() {
     return (
         <ProviderComponent>
-            {/* <Test /> */}
             <SearchSection />
             <ResultsSection />
         </ProviderComponent>
