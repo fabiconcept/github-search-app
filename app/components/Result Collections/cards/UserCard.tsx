9a8906fceed1f87afe17ab39d1dc2ca593c342c3
fabiconcept <favourajokubi@gmail.com>
2024-12-04 13:12:37 +0100

    Update UserCard component to wrap content in a Link component, change background and border colors, and adjust styles for hover and dark mode.

--- app/components/Result Collections/cards/UserCard.tsx
+++ app/components/Result Collections/cards/UserCard.tsx
@@ -15,7 +15,7 @@ interface UserCardParams {
 
 export default function UserCard(params:UserCardParams) {
     return (
-        <div className="p-2 min-w-[15rem] flex-1 sm:max-w-[15rem] max-w-[18rem] bg-black/20 shadow-lg border border-black/10 dark:border-white/10 hover:border-green-700/50 hover:bg-black/5 dark:hover:bg-white/5 active:scale-95 hover:shadow-green-600/10 rounded-lg text-xs">
+        <Link href={`/user/${params.login}`} className="group p-2 min-w-[15rem] flex-1 sm:max-w-[15rem] max-w-[18rem] bg-white shadow-lg border border-black/10 dark:border-white/10 hover:border-green-700/50 hover:bg-white/80 dark:hover:bg-white/5 active:scale-95 hover:shadow-green-600/10 rounded-lg text-xs">
             <div className="overflow-hidden rounded-lg w-full aspect-auto grid place-items-center relative">
                 <Image
                     src={params.avatar_img}
@@ -25,21 +25,21 @@ export default function UserCard(params:UserCardParams) {
                     className="w-full sm:h-[10rem] object-cover"
                     priority
                 />
-                <div className="absolute top-0 h-full w-full bg-gradient-to-t from-[rgb(15,15,15)] via-transparent">
+                <div className="absolute top-0 h-full w-full bg-gradient-to-t dark:from-[rgb(15,15,15)] from-[rgb(15,15,15,0.2)] via-transparent">
 
                 </div>
             </div>
-            <div className="mx-3 py-3 flex items-center justify-between border-b border-white/10">
+            <div className="mx-3 py-3 flex items-center justify-between border-b dark:border-white/10 border-black/10">
                 <span className="flex gap-1 w-full">
                     <span className="text-green-500 font-semibold max-w-[90%] truncate">@{params.site_admin ? "You" : params.login}</span>
                 </span>
-                <Link href={`/user/${params.login}`} className="cursor-pointer hover:text-green-500 hover:scale-125" title="Open in GitHub">
+                <span className="cursor-pointer group-hover:text-green-500 group-hover:scale-125" title="Open in GitHub">
                     <FaArrowUpRightFromSquare />
-                </Link>
+                </span>
             </div>
             <div className="mx-3 py-3 flex items-center text-center justify-center uppercase last:border-none border-b border-white/10 opacity-50  text-[0.65rem]">
                 <span>{params.type}</span>
             </div>
-        </div>
+        </Link>
     )
 }
